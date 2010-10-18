b5116b47ca20b39025c1dc3a6a3caad48605f707
Virginia Cihlar <vcihlar@graebel.com>
2010-10-18 15:14:34 -0500

    saving now

--- product/nothinbutdotnetprep/collections/MovieLibrary.cs
+++ product/nothinbutdotnetprep/collections/MovieLibrary.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace nothinbutdotnetprep.collections
 {
     public class MovieLibrary
-    {
+    { 
         IList<Movie> movies;
 
         public MovieLibrary(IList<Movie> list_of_movies)
@@ -13,28 +13,45 @@ namespace nothinbutdotnetprep.collections
         }
 
         public IEnumerable<Movie> all_movies()
-        {
-            return movies;
+        { // original
+          // return movies;
+            foreach (var movie in movies) yield return movie;
+           
         }
 
         public void add(Movie movie)
         {
-            throw new NotImplementedException();
+            if (movies.Contains(movie))
+                return;
+            
+            foreach (var m in movies )
+
+            {
+                if (m.title == movie.title)
+                    return;
+            }
+            movies.Add(movie);
         }
 
         public IEnumerable<Movie> sort_all_movies_by_title_descending
         {
             get { throw new NotImplementedException(); }
+            
         }
 
         public IEnumerable<Movie> all_movies_published_by_pixar()
         {
-            throw new NotImplementedException();
+            foreach (var mx in movies)
+                if (mx.production_studio == ProductionStudio.Pixar)
+                    yield return mx;
         }
 
         public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
         {
-            throw new NotImplementedException();
+            foreach (var mx in movies)
+                if ((mx.production_studio == ProductionStudio.Pixar) ||
+            (mx.production_studio == ProductionStudio.Disney))
+                    yield return mx;
         }
 
         public IEnumerable<Movie> sort_all_movies_by_title_ascending
